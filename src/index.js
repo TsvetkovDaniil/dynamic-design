6d9419294bfa579d142b4df5932a1c7577f3965f
Tsvetkov Daniil <117004775+TsvetkovDaniil@users.noreply.github.com>
2023-04-16 18:56:45 +0400

    created all themes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './themes/light.css'
 import './themes/dark.css'
+import './themes/dark-blue.css'
+import './themes/dark-red.css'
+import './themes/rosdistant.css'
+import './themes/portal.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
