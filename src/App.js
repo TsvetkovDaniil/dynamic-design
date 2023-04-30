cdb95c8a1bbf46204979fc918fbe46ec689f8c72
Tsvetkov Daniil <117004775+TsvetkovDaniil@users.noreply.github.com>
2023-04-30 22:29:06 +0400

    finished dynamic domain system

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useEffect } from 'react'
 import './App.css'
 import Portal from './pages/Portal/Portal'
 import Rosdistant from './pages/Rosdistant/Rosdistant'
 
 function App() {
+  const domain = window.location.hostname
+
   return (
     <div className="App">
-      <Portal />
+      {domain === 'rosdistant.local' ? (
+        <Rosdistant />
+      ) : domain === 'portal.local' ? (
+        <Portal />
+      ) : (
+        <Rosdistant />
+      )}
     </div>
   )
 }
